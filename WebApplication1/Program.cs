2881c016c1b14c5335f0183804a15e557eba36e3
Danila <61559724+Saxez@users.noreply.github.com>
2023-05-17 19:09:38 +0300

    we

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -202,7 +202,7 @@ App.MapPost("/send_code", async (HttpRequest Request) =>
     // PassSender.SendMessage(Email, "Код для восстановления пароля:" + code + ". Действует только 5 минут", "Код для восстановления пароля");
     var test = Encoding.UTF8.GetString(Encoding.Default.GetBytes("русское сообщение"));
     PassSender.SendMessage(Email, test, test);
-    Console.OutputEncoding = Encoding.UTF8;
+    Console.OutputEncoding = Encoding.Unicode;
     Console.WriteLine(test);
     return Results.Ok();
 });
