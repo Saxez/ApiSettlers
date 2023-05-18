2b8f9bb4751a1f9450f05ef2e76c63b06c06d645
Danila <61559724+Saxez@users.noreply.github.com>
2023-05-18 22:29:05 +0300

    e2

--- WebApplication1/Email/PassSender.cs
+++ WebApplication1/Email/PassSender.cs
@@ -11,7 +11,7 @@ namespace Project1.Email
         public static void SendMessage(string adressTo, string messageText, string theme)
         {
             MimeMessage mes = new MimeMessage();
-            mes.From.Add(new MailboxAddress(theme, "Settlerreg@yandex.ru"));
+            mes.From.Add(new MailboxAddress("Регистрация в системе", "Settlerreg@yandex.ru"));
             mes.To.Add(MailboxAddress.Parse(adressTo));
             mes.Subject = "Your password to account";
             mes.Body = new TextPart("plain")
